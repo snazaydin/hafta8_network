3ed2207ea64cd17fe6a859915a115273419c805c
snazaydin <sudenaz52aydin@gmail.com>
2026-03-24 06:40:56 -0400

    Implement basic TCP server in server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = "10.17.31.250"  # Standard loopback interface address (localhost)
+PORT = 65428  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+SUDE NAZ AYDIN
+
+
+
